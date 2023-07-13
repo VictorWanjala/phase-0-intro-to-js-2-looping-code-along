7b813661f43bc3c157b13a177763c043d29ed84e
Victor <wanjala94vic@gmail.com>
2023-07-14 00:36:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const messages =writeCards(['Guadalupe', 'Ollie', 'Aki'],'surprise');
+function writeCards(names, eventName){
+    
+    let messages = [];
+    for(let i=0;i<names.length;i++){
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+    return messages;
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
